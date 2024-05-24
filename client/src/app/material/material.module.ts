fe8b5372d88783bd8165245365b4d4d0db923c97
juvin vinod <124287134+Juvinvinod@users.noreply.github.com>
2024-05-25 01:19:41 +0530

    Added material module

---
+++ client/src/app/material/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [],
+  exports: [CommonModule, FormsModule, MatToolbarModule],
+})
+export class MaterialModule {}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
   exports: [NavbarComponent],
 })
 export class SharedModule {}
