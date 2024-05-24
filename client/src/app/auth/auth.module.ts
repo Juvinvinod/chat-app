bc3e23ffadfe83968ab413e852a6e90908252e73
juvin vinod <124287134+Juvinvinod@users.noreply.github.com>
2024-05-25 03:25:24 +0530

    Added login component

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, SharedModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    AuthRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AuthModule {}
--- client/src/app/material/material.module.ts
+++ client/src/app/material/material.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
-  exports: [CommonModule, FormsModule, MatToolbarModule],
+  exports: [
+    CommonModule,
+    FormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
 })
 export class MaterialModule {}
